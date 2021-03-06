8aa0d423734b4ba52767169436e2d51f02b41adc
sugurunnba <sugurunnba7@gmail.com>
2021-03-06 02:42:08 +0000

    Addition of function => Create a list page of unsubscribed users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,9 +1,13 @@
 class Admin::UsersController < ApplicationController
-  before_action :authenticate_admin!,
+  before_action :authenticate_admin!
 
-                def index
-                  @users = User.page(params[:page]).reverse_order
-                end
+  def leave
+    @users = User.page(params[:page]).reverse_order
+  end
+
+  def index
+    @users = User.page(params[:page]).reverse_order
+  end
 
   def show
     @user = User.find(params[:id])
